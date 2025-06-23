5a1a2a73cf586e51c0f7392929df779123061953
skkhandokar <khandokarsamad4@gmail.com>
2025-06-23 22:20:28 +0600

    updated

--- src/app/head.js
+++ src/app/head.js
@@ -1,10 +1,23 @@
 
+
 export default function Head() {
   return (
     <>
-      <title>Short Url</title>
-      <meta name= "Shortfy" content="Shortfy helps you create clean, short URLs for YouTube, WhatsApp, Instagram, and any website—fast, free, and easy to use." />
-     
+      <title>Shortfy Free Short URL Generator </title>
+      <meta name="description" content="Shortfy helps you create clean, short URLs for YouTube, WhatsApp, Instagram, and any website—fast, free, and easy to use." />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="theme-color" content="#ffffff" />
+      <meta name="application-name" content="Shortfy" />
+      <meta name="apple-mobile-web-app-title" content="Shortfy" />
+
+      {/* Favicon and Icons */}
+      <link rel="icon" href="/favicon.ico" sizes="any" />
+      <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="48x48" href="/favicon-48x48.png" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      <link rel="manifest" href="/site.webmanifest" />
     </>
   );
 }
