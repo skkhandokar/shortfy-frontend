07a683feaa696db4aa4476b45729c5273140ddee
Khandokar Samadul Kabir <111075442+skkhandokar@users.noreply.github.com>
2025-07-20 03:08:07 +0600

    updated

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,6 +4,7 @@ import Navbar from "./components/Navbar"
 import Footer from "./components/Footer"
 import { AuthProvider } from "./context/AuthContext"
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] })
 const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"] })
