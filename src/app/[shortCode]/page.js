e64d8ed3ca48576e1fd3eeb1f0f9897eddb17c86
Khandokar Samadul Kabir <111075442+skkhandokar@users.noreply.github.com>
2025-12-27 01:58:24 +0600

    Update short.js

--- src/app/[shortCode]/page.js
+++ src/app/[shortCode]/page.js
@@ -1,20 +1,45 @@
-// app/[shortCode]/page.js
 
-export default function RedirectPage({ params }) {
-  const { shortCode } = params;
+
+// export default function RedirectPage({ params }) {
+//   const { shortCode } = params;
+
+//   return (
+//     <html>
+//       <body>
+//         <p>Redirecting...</p>
+//         <script
+//           dangerouslySetInnerHTML={{
+//             __html: `
+//               window.location.href = 'https://skkhandokar22.pythonanywhere.com/api/check/${shortCode}/';
+//             `,
+//           }}
+//         />
+//       </body>
+//     </html>
+//   );
+// }
+
+
+
+
+
+
+
+export default async function RedirectPage({ params }) {
+  // params এখন একটি Promise, তাই await করতে হবে
+  const { shortCode } = await params;
 
   return (
-    <html>
-      <body>
-        <p>Redirecting...</p>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.location.href = 'https://skkhandokar22.pythonanywhere.com/api/check/${shortCode}/';
-            `,
-          }}
-        />
-      </body>
-    </html>
+    <div>
+      <p>Redirecting...</p>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.location.href = 'https://skkhandokar22.pythonanywhere.com/api/check/${shortCode}/';
+          `,
+        }}
+      />
+    </div>
   );
 }
+
